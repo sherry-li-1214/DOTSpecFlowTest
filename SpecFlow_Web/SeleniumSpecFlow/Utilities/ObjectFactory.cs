af8f48c7955b4231ea3a87eff5fe1ab8a5b83508
sherry-li-1214 <19400030+sherry-li-1214@users.noreply.github.com>
2022-02-13 23:13:53 +1100

    Update ObjectFactory.cs

--- SpecFlow_Web/SeleniumSpecFlow/Utilities/ObjectFactory.cs
+++ SpecFlow_Web/SeleniumSpecFlow/Utilities/ObjectFactory.cs
@@ -10,6 +10,7 @@ namespace SeleniumSpecFlow.Utilities
         public Lazy<HomePage> HomePage = new Lazy<HomePage>(() => new HomePage(Hooks.Driver));
         public Lazy<QuestionsListPage> QuestionsListPage = new Lazy<QuestionsListPage>(() => new QuestionsListPage(Hooks.Driver));
         public Lazy<ContactInfoPage> ContactInfoPage = new Lazy<ContactInfoPage>(() => new ContactInfoPage(Hooks.Driver));
+        public Lazy<SuccessPage> SuccessPage = new Lazy<SuccessPage>(() => new SuccessPage(Hooks.Driver));
 
     }
 
